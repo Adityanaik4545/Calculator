d9d102aa525b2e2d8e27b16a33e11069ef05bb14
Adityanaik4545 <adityank23@outlook.com>
2025-04-16 02:07:53 +0530

    made changes in css

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ allButtons.forEach(e=>{e.addEventListener("click",function(event){
     let pressedButton= event.target.innerText;
     if (pressedButton== "ac") {
         string="";
-        screen.value=""
-        console.log("AC pressed, string reset: ", string); // Log when AC is pressed
+        screen.value="";
+        
 
     }
     else if(pressedButton=="="){
         string=resultCooked(string);
         screen.value=string;
-        console.log("result after : ", string); // Log when AC is pressed
 
     }
     else if(pressedButton=="del"){
         string=string.slice(0,string.length-1)
-        screen.value=string
-        console.log("del pressed, string reset: ", string); // Log when AC is pressed
+        screen.value=string;
 
     }
     else{
         string=string+pressedButton;
         screen.value=string;
+        
+
     }
 })})
 function resultCooked(input) {
