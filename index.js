d6ef01d7fb0c524c03f7747d332282e4a76beb70
Adityanaik4545 <adityank23@outlook.com>
2025-04-15 23:59:18 +0530

    made changes in logic

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ allButtons.forEach(e=>{e.addEventListener("click",function(event){
     let pressedButton= event.target.innerText;
     if (pressedButton== "ac") {
         string="";
+        screen.value=""
+        console.log("AC pressed, string reset: ", string); // Log when AC is pressed
+
+    }
+    else if(pressedButton=="="){
+        string=resultCooked(string);
+        screen.value=string;
+        console.log("result after : ", string); // Log when AC is pressed
+
+    }
+    else if(pressedButton=="del"){
+        string=string.slice(0,string.length-1)
+        screen.value=string
+        console.log("del pressed, string reset: ", string); // Log when AC is pressed
+
     }
     else{
         string=string+pressedButton;
-        
-    }
-    if(pressedButton=="="){
-        eval(string)
+        screen.value=string;
     }
 })})
+function resultCooked(input) {
+    return new Function(`return ${input}`)();
+}
