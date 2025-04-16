1a4d0c9ce49e092dbd8832d29fd4234626db147e
Adityanaik4545 <adityank23@outlook.com>
2025-04-16 21:25:42 +0530

    1.removed eval() and made function for processing result and to ensure safety measure,2. modified code for "=" button if user press "=" with empty input it will show "error" message if user clicks any other button after this. screen will get reset and add current clicked button value on screen,3. added function where if user clicks "." in empty screen it will show "0." for better user experience,4.fixed bugs for operator button which restricts user to input multiple operators at onces.

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 let screen=document . getElementById("value");
 let allButtons=document.querySelectorAll("button");
 let string="";
+let isError=false;
 allButtons.forEach(e=>{e.addEventListener("click",function(event){
     let pressedButton= event.target.innerText;
-    if (pressedButton== "ac") {
+    let operator=["/","*","-","%","+"];
+    let lastChar=string[string.length-1];
+    if (isError && pressedButton!=="AC") {
+        string="";
+        screen.value="";
+        isError=false;
+        
+    }
+    if (operator.includes(pressedButton)) {
+        if (string==="" || operator.includes(lastChar)) {
+            return;
+        }
+        else{
+            string=string+pressedButton;
+            screen.value=string;
+            return;
+        }
+        
+    } 
+    if (pressedButton== "AC") {
         string="";
         screen.value="";
         
 
     }
     else if(pressedButton=="="){
-        string=resultCooked(string);
-        screen.value=string;
+        if (string=="") {
+            screen.value="error";
+            isError=true;
+            return;
+        }
+        else{
+
+            string=resultCooked(string);
+            screen.value=string;
+        }
+            
 
     }
     else if(pressedButton=="del"){
         string=string.slice(0,string.length-1)
         screen.value=string;
 
+    }
+    else if(pressedButton=="."){
+        if (string=="") {
+            string=`0${pressedButton}`;
+            screen.value=string;
+        }
+        else{
+
+            let parts= string.split(/[\+\-\*\/]/);
+            currentNumber=parts[parts.length-1];
+            if (!currentNumber.includes(".")) {
+                string=string+pressedButton;
+                screen.value=string;
+            }
+        }
     }
     else{
         string=string+pressedButton;
