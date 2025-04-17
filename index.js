28f47676052dcecaff33b4cb7c00fae6cce00459
Adityanaik4545 <adityank23@outlook.com>
2025-04-17 19:59:43 +0530

    enabled keyboard support (press "c" to clear all)

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let screen=document . getElementById("value");
 let allButtons=document.querySelectorAll("button");
 let string="";
 let isError=false;
+let operator=["/","*","-","%","+"];
 allButtons.forEach(e=>{e.addEventListener("click",function(event){
     let pressedButton= event.target.innerText;
-    let operator=["/","*","-","%","+"];
     let lastChar=string[string.length-1];
     if (isError && pressedButton!=="AC") {
         string="";
@@ -70,6 +70,67 @@ allButtons.forEach(e=>{e.addEventListener("click",function(event){
 
     }
 })})
+document.addEventListener("keydown",function(event){
+let pressedKey=event.key;
+let lastChar=string[string.length-1];
+let number=["0","1","2","3","4","5","6","7","8","9"];
+event.preventDefault();
+if (pressedKey==="Enter") {
+    if (string=="") {
+        screen.value="error";
+        isError=true;
+        return;
+    }
+    else{
+
+        string=resultCooked(string);
+        screen.value=string;
+        return
+    }
+} 
+else if(number.includes(pressedKey)){
+    string=string+pressedKey;
+    screen.value=string;
+    return
+}
+else if(operator.includes(pressedKey)) {
+    if (string==="" || operator.includes(lastChar)) {
+        return;
+    }
+    else{
+        string=string+pressedKey;
+        screen.value=string;
+        return;
+    }
+    
+}
+else if(pressedKey==="Backspace") {
+    string=string.slice(0,string.length-1)
+    screen.value=string;
+}
+else if(pressedKey==="."){
+    if (string=="") {
+        string=`0${pressedKey}`;
+        screen.value=string;
+    }
+    else{
+
+        let parts= string.split(/[\+\-\*\/]/);
+        currentNumber=parts[parts.length-1];
+        if (!currentNumber.includes(".")) {
+            string=string+pressedKey;
+            screen.value=string;
+        }
+    }
+}
+else{
+    if (pressedKey==="c") {
+        
+        string="";
+            screen.value="";
+    }
+}
+})
 function resultCooked(input) {
     return new Function(`return ${input}`)();
 }
